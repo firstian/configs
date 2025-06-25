0d42a35b4db7f716860f8f223faea6ee6f454553
Joe Chan <firstianus@gmail.com>
2025-06-25 12:13:02 -0400

    Update LazyVim and keymap

--- vim/nvim/lua/config/keymaps.lua
+++ vim/nvim/lua/config/keymaps.lua
@@ -5,10 +5,10 @@
 local keymap = vim.keymap
 
 -- TIP: Disable arrow keys in normal mode
-keymap.set({ "n", "i" }, "<left>", '<cmd>echo "Use h to move!!"<CR>')
-keymap.set({ "n", "i" }, "<right>", '<cmd>echo "Use l to move!!"<CR>')
-keymap.set({ "n", "i" }, "<up>", '<cmd>echo "Use k to move!!"<CR>')
-keymap.set({ "n", "i" }, "<down>", '<cmd>echo "Use j to move!!"<CR>')
+keymap.set({ "n" }, "<left>", '<cmd>echo "Use h to move!!"<CR>')
+keymap.set({ "n" }, "<right>", '<cmd>echo "Use l to move!!"<CR>')
+keymap.set({ "n" }, "<up>", '<cmd>echo "Use k to move!!"<CR>')
+keymap.set({ "n" }, "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 --  See `:help wincmd` for a list of all window commands
 -- keymap.set({"n", "i"}, "<M-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
