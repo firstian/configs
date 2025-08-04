cf7c4f82b9c050af498b817cb036b0953d1795aa
Joe Chan <firstianus@gmail.com>
2025-08-04 15:28:24 -0400

    Update instruction after testing.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,7 +1,19 @@
 #! /usr/bin/env bash
 
 stow --dotfile git
+
+if [[ -f ~/.zprofile ]]; then
+  mv ~/.zprofile ~/.zprofile.bak
+fi
+
+if [[ -f ~/.zshrc ]]; then
+  mv ~/.zshrc ~/.zshrc.bak
+fi
+
 stow --dotfile zsh
+
+# Make sure .config dir exists, or stow will fail.
+mkdir -p ~/.config
 stow dot-config -t ~/.config
 
 # These need to be customize anyway.
